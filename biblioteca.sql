e5546bf4c5faf445477f0af2506b700f04c24f2b
gialbanes <giovanaalbanes@gmail.com>
2024-08-31 14:25:49 -0300

    exercício da biblioteca completo

--- biblioteca.sql
+++ biblioteca.sql
@@ -82,6 +82,7 @@ de Odisseu e sua jornada de volta para casa.' ),
 (9780201616224, 'Design Patterns', 'Elements of Reusable Object-Oriented Software', 1994, 
 'Tecnologia', 'Padrões de design de software e suas aplicações em programação orientada a 
 objetos.' );
+
 select * from livros;
 
 insert into autores (nomeCompleto, dataNasc, biografiaResumida) values
@@ -97,54 +98,111 @@ insert into livrosXautores (codLivro, codAutor) values
 (2,2),
 (3,3);
 
+select * from livrosxautores;
+
 insert into usuarios (nomeCompleto, endereco, bairro, cidade, UF, assinatura) values
 ('Ana Silva', 'Rua das Flores, 123', 'Jardim Primavera', 'São Paulo', 'SP', 'Aluno'),
 ('João Souza', 'Avenida Central, 456', 'Centro', 'Rio de Janeiro', 'RJ', 'Professor'),
 ('Maria Oliveira', 'Praça da Liberdade, 789', 'Liberdade', 'Belo Horizonte', 'MG', 'Funcionario');
 
+select * from usuarios;
+
 insert into usuariosXtelefone (codUsuario, telefone) values
-(1,11987654321),
-(2, 21987654321 ),
-(3, 31987654321);
+(6,11987654321),
+(7, 21987654321 ),
+(8, 31987654321);
 
 select * from usuariosXtelefone;
 
 insert into usuariosXemails (codUsuario, email) values
-(1, 'ana.silva@example.com'),
-(2, 'joao.souza@example.com'),
-(3, 'maria.oliveira@example.com');
+(6, 'ana.silva@example.com'),
+(7, 'joao.souza@example.com'),
+(8, 'maria.oliveira@example.com');
 
-alter table emprestimos add column codUsuario int,
-add constraint  fk_codUsuario_emprestimos foreign key(codUsuario) references usuarios(codUsuario);
+select * from usuariosxemails;
 
 insert into emprestimos (dtEmprestimo, dtPrevistaDevolucao, dtRealDevolucao, codUsuario) values
-('2024-08-01', '2024-08-15', NULL, 1),
-('2024-08-05', '2024-08-20', NULL, 2),
-('2024-08-10', '2024-08-25', NULL, 3);
+('2024-08-01', '2024-08-15', NULL, 6),
+('2024-08-05', '2024-08-20', NULL, 7),
+('2024-08-10', '2024-08-25', NULL, 8);
+
+select * from emprestimos;
 
 insert into emprestados (codLivro, codEmprestimo) values 
-(1,1),
-(2,2),
-(3,3);
+(1,7),
+(2,8),
+(3,9);
 
+select * from emprestados;
 
 #1. Listar todos os livros disponíveis na biblioteca
 select * from livros;
 
 #2. Encontrar todos os autores que têm mais de 50 anos
-select * from autores;
-SELECT nomeCompleto, FLOOR(DATEDIFF(CURDATE(), dataNasc) / 365) AS idade FROM autores WHERE FLOOR(DATEDIFF(CURDATE(), dataNasc) / 365) > 50;
+select nomeCompleto as Autor, FLOOR(DATEDIFF(CURDATE(), dataNasc) / 365) as Idade from autores where FLOOR(DATEDIFF(CURDATE(), dataNasc) / 365) > 50;
 
 
 #3. Mostrar todos os usuários que têm assinatura de 'Professor'
 select * from usuarios where assinatura='Professor';
 
-#4. Listar todos os emprestimos que ainda não foram devolvidos.
-select * from emprestimos;
-SELECT codEmprestimo, dtEmprestimo, dtPrevistaDevolucao, TIMESTAMPDIFF(DAY , dtEmprestimo, dtPrevistaDevolucao) AS 'DIAS MÁX DE EMPRÉSTIMO' FROM emprestimos WHERE TIMESTAMPDIFF(DAY, dtEmprestimo, dtPrevistaDevolucao) = 15;
+#4. Listar todos os emprestimos que ainda não foram devolvidos. 
+select e.codEmprestimo as Empréstimo, dtEmprestimo as 'Data de empréstimo', dtRealDevolucao as 'Data real de devolução' from emprestados ep
+inner join emprestimos e on ep.codEmprestimo = e.codEmprestimo 
+where dtRealDevolucao is null;
 
 #5. Exibir todos os livros junto com os nomes dos autores que os escreveram.
 select l.titulo as Livro, a.nomeCompleto as Autor from livrosxautores
 inner join livros l on livrosxautores.codLivro = l.codLivro
 inner join autores a on livrosxautores.codAutor = a.codAutor;
+
+#6. Mostrar todos os empréstimos com o nome do usuário que fez o empréstimo e o título do livro emprestado.
+select e.codEmprestimo as Empréstimo, u.nomeCompleto as Usuario, l.titulo as Livro from emprestimos e
+inner join emprestados ep on e.codEmprestimo = ep.codEmprestimo
+inner join usuarios u on e.codUsuario = u.codUsuario
+inner join livros l on ep.codLivro = l.codLivro;
+
+#7. Listar todos os usuários e seus números de telefone.
+select u.nomeCompleto as Usuario, telefone from usuariosxtelefone
+inner join usuarios u on usuariosxtelefone.codUsuario = u.codUsuario;
+
+#8. Mostrar todos os e-mails dos usuários que têm assinatura 'Funcionario'.
+select u.nomeCompleto as Usuario, email as Email from usuariosxemails
+inner join usuarios u on usuariosxemails.codusuario = u.codUsuario where assinatura = 'Funcionário';
+
+#9.Encontrar todos os livros publicados após 2000 e ordená-los pelo ano de publicação. 
+select titulo as Título, subtitulo as Subtítulo, ano as 'Ano de publicação' from livros order by ano desc;
+
+#10. Listar todos os autores cuja biografia contém a palavra 'design'.
+select nomeCompleto as Autor, biografiaResumida as Biografia from autores where biografiaResumida like '%_design_%';
+
+#11. Mostrar os detalhes dos empréstimos que foram feitos por usuários da cidade 'São Paulo'.
+select u.nomeCompleto as Usuario , u.cidade as Cidade, l.titulo as Livro, dtEmprestimo as 'Data do empréstimo', dtPrevistaDevolucao as 'Data prevista de devolução', 
+dtRealDevolucao as 'Data real de devolução' from emprestimos e
+inner join emprestados ep on e.codEmprestimo = ep.codEmprestimo
+inner join livros l on ep.codLivro = l.codLivro
+inner join usuarios u on e.codUsuario = u.codUsuario where cidade = 'São Paulo';
+
+#12.Contar o número de livros de cada gênero na biblioteca.
+select genero as Gênero, count(*) as 'Quantidade de livros' from livros group by genero;
+
+#13. Encontrar o autor com o maior número de livros na biblioteca.
+insert into livros (ISBN, titulo, subtitulo, ano, genero, descricao) values
+(1234567891234, 'Teste', 'Teste', 900, 'Teste', 'Teste para o exercício 13');
+
+insert into livrosXautores (codLivro, codAutor) values
+(13,1);
+
+select a.nomeCompleto as Autor, count(*) as 'Quantidade de livros' from livrosxautores
+inner join autores a on livrosxautores.codAutor = a.codAutor
+group by a.nomeCompleto;
+
+#14. Mostrar todos os usuários que nunca fizeram um empréstimo.
+select * from emprestimos where codusuario is null;
+
+#15.Listar os livros que foram emprestados, mas ainda não foram devolvidos.
+select l.titulo as Livro, dtEmprestimo as 'Data de empréstimo', dtRealDevolucao as 'Data real de devolução' from emprestados ep
+inner join livros l on ep.codLivro = l.codLivro
+inner join emprestimos e on ep.codEmprestimo = e.codEmprestimo
+where e.dtRealDevolucao is null;
+
 
