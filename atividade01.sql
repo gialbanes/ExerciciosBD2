d035b08be6e0b1d024e65cebc86c1009b877d1d0
gialbanes <giovanaalbanes@gmail.com>
2024-09-20 13:53:55 -0300

    finalizando a atividade 1

--- atividade01.sql
+++ atividade01.sql
@@ -1,5 +1,5 @@
 create database if not exists emptech_giovana;
-
+ 
 use emptech_giovana;
 
 create table if not exists funcionarios(
@@ -40,40 +40,6 @@ insert into veiculos(modelo, placas, codFunc) values
 ('Golf', 'TRE3M65', 9),
 ('Fusca', 'ASD3Q12', 8); 
 
-#inner join
-select f.nomeFunc as Nome, veiculos.modelo as Modelo from veiculos
-inner join funcionarios f on veiculos.codFunc = f.codFunc;
-
-#equi join
-select f.nomeFunc as Nome, v.modelo from veiculos v  
-join funcionarios f using(codFunc);
-
-#left join: retorna todos os campos do lado esquerdo do Join que se relaciona com o lado direito, mais os registros que não que não relacionam com o lado direito e que sejam do lado esquerdo; a diferença é que trouxe o carro que não tem dono; listo tudo que tenha ou não relacionamento da tabelaA com a tabela tabelaB
-select f.nomeFunc as Nome, v.modelo from veiculos v
-left join funcionarios f using (codFunc);
-
-#right join: retorna todos os campos do lado direito do Join que se relaciona com o lado esquerdo, mais os registros que não que não relacionam com o lado esquerdo e que sejam do lado direito; a diferença é que trouxe o carro que não tem dono; listo tudo que tenha ou não relacionamento da tabelaB com a tabela tabelaA
-select f.nomeFunc as Nome, v.modelo from veiculos v
-right join funcionarios f using (codFunc);
-
-#full join:  o MySQL não tem o full, logo fazemos o LEFT E RIGHT na mesma query, porém utilizar o UNION; cruza os dados, ou seja, quem tem ou não da esquerda com a direita, e quem tem ou não da direita com a esquerda; posso fazer quantas UNION forem necessárias
-select f.nomeFunc as Nome, v.modelo from veiculos v
-left join funcionarios f using (codFunc)
-union
-select f.nomeFunc as Nome, v.modelo from veiculos v
-right join funcionarios f using (codFunc);
-
-#view: estrutura de seleção que encapsula querys complexas para simplificar o uso ao usuário e facilitar as chamdas em aplicações externas
-create view func_veic as 
-select f.nomeFunc as Nome, v.modelo from veiculos v
-left join funcionarios f using (codFunc)
-union
-select f.nomeFunc as Nome, v.modelo from veiculos v
-right join funcionarios f using (codFunc);
-
-#chamando a view
-select * from func_veic;
-
 create table atuacaoVendas(
 codAtuacao int auto_increment primary key,
 descricao varchar(255) not null
@@ -90,15 +56,6 @@ insert into atuacaoVendas(descricao) values
 ('Consultoria de vendas'),
 ('Programações e eventos especiais');
 
-
-#cross join: irá criar relatório onde irá fazer todas as combinações possíveis entre as tabelas; para cada linha da tabela esquerda queremos todos os linhas da tabelas direita ou vice-versa; ele também é chamado de produto cartesiano entre duas tabelas; porém, para isso é preciso que ambas tenham o campo em comum, para que a ligação exista entre as duas tabelas.
-/*Ex: se cruzarmos as tabelas funcionario, veiculos e atuacaoVendas onde teremos F com 6 registros, V com & e AV com 6, iremos um resultado de combinação 6 X 7 X 6 que totalizará combinações*/
-
-select f.codFunc as Funcionário, v.modelo as Modelo, v.placas as Placa, a.descricao as Descrição from funcionarios f
-cross join veiculos v 
-cross join atuacaoVendas a;
-
-#autorelacionamento: quem indicou quem, no caso, funcionários
 create table indicacoes(
 codIndicador int,
 codIndicado int,
@@ -106,8 +63,6 @@ primary key(codIndicador, codIndicado),
 foreign key (codIndicador) references funcionarios(codFunc),
 foreign key (codIndicado) references funcionarios(codFunc)
 );
-
-select * from indicacoes;
 
 insert into indicacoes(codIndicador, codIndicado) values
 (1,2),
@@ -119,14 +74,8 @@ insert into indicacoes(codIndicador, codIndicado) values
 (9,7),
 (8,9);
 
-#self join: gera um resultado de relacionamento de dados de uma tabela com ela mesma, ou seja, um auto-relacionamento
-select i1.codIndicador as "ID indicador", f1.nomeFunc as "Nome indicador", 
-i1.codIndicado as "ID indicado", f2.nomeFunc as "Nome indicado" from indicacoes i1
-join funcionarios f1 on i1.codIndicador = f1.codFunc
-join funcionarios f2 on i1.codIndicado = f2.codFunc;
-
-
-#1. Crie uma função chamada `GetFuncionarioVeiculoCount` que recebe o código de um funcionário e retorna o número de veículos associados a esse funcionário.
+/*1. Crie uma função chamada `GetFuncionarioVeiculoCount` que recebe o código de um funcionário e retorna o 
+número de veículos associados a esse funcionário.*/
 Delimiter //
 create function getFuncionarioVeiculoCount(id int) returns int
 begin
@@ -136,9 +85,11 @@ return ifnull(veiculos, 0);
 end // 
 Delimiter ;
 
-select getFuncionarioVeiculoCount(1)  as 'Quantidade de veículos' ;
+select getFuncionarioVeiculoCount(8)  as 'Quantidade de veículos' ;
+select * from veiculos;
 
-#1. Crie um procedimento armazenado chamado `AddVenda` que insere uma nova atuação de vendas na tabela `AtuacaoVendas`. O procedimento deve receber uma descrição e adicionar a nova atuação de vendas.
+/*1. Crie um procedimento armazenado chamado `AddVenda` que insere uma nova atuação de vendas na tabela `AtuacaoVendas`.
+ O procedimento deve receber uma descrição e adicionar a nova atuação de vendas.*/
 delimiter //
 create procedure addVenda(in id int, in descricao varchar(255))
 begin
@@ -151,7 +102,8 @@ select * from atuacaoVendas;
 call addVenda(10, "Teste");
 
 
-#3. Crie um gatilho chamado `BeforeInsertIndicacao` que verifica se o funcionário indicado já foi indicado por outro funcionário. Se o funcionário já tiver uma indicação, o gatilho deve lançar um erro e impedir a inserção
+/*3. Crie um gatilho chamado `BeforeInsertIndicacao` que verifica se o funcionário indicado já foi indicado por outro funcionário. 
+Se o funcionário já tiver uma indicação, o gatilho deve lançar um erro e impedir a inserção*/
 delimiter //
 create trigger beforeInsertIndicacao
 before insert on indicacoes
@@ -164,16 +116,22 @@ begin
 end
 // delimiter ;
  
-insert into indicacoes (codIndicador, codIndicado) values (3, 1);
+insert into indicacoes (codIndicador, codIndicado) values (2, 8);
 
 select * from indicacoes;
 
-#4. 
+/*3. Crie uma visão chamada `FuncionarioVeiculoAtuacao` que exibe todos os funcionários, seus veículos e as atuações de vendas associadas.
+ A visão deve mostrar os campos: código do funcionário, nome do funcionário, modelo do veículo, placas do veículo e descrição da atuação de vendas.*/
 create view funcionarioVeiculoAtuacao as 
-select f.codFunc as "ID do funcionário", f.nomeFunc as "Nome do funcionário", v.modelo as "Modelo", v.placas as "Placa", a.descricao as "Descriação da atuação vendas" from veiculos v
-inner join funcionarios f on v.codFunc = f.codFunc
-inner join atuacaoVendas a on v.;
+select f.codFunc as "ID do funcionário", f.nomeFunc as "Nome do funcionário", v.modelo as "Modelo do veículo", v.placas as "Placa do veículo", a.descricao as "Descriação da atuação vendas" from funcionarios f
+inner join veiculos v on f.codFunc = v.codFunc 
+inner join atuacaoVendas a on f.codAtuacao = a.codAtuacao;
 
-#chamando a view
-select * from func_veic;
+select * from funcionarioVeiculoAtuacao;
+
+/*3. Execute uma consulta para mostrar a lista completa de funcionários, indicando se eles têm veículos e quais atuações de vendas estão associadas a cada veículo, 
+usando um `LEFT JOIN` para incluir todos os funcionários, mesmo aqueles sem veículos ou atuações de vendas.*/
+select f.codFunc as "ID do funcionário", f.nomeFunc as "Nome do funcionário", v.modelo as "Modelo do veículo", a.descricao as "Atuação de vendas" from funcionarios f 
+left join veiculos v on f.codFunc = v.codFunc
+left join atuacaoVendas a on f.codAtuacao = a.codAtuacao;
 
